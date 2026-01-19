3a592bdff036010a5119b53a8ba21f0889154e72
cristalavera <crispipop@gmail.com>
2026-01-19 20:03:13 +0100

    Preparado backend con variables de entorno y configuración

--- src/main/java/com/electroshop/ElectroShopApplication.java
+++ src/main/java/com/electroshop/ElectroShopApplication.java
@@ -3,10 +3,18 @@ package com.electroshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ElectroShopApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // lee .env automáticamente
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
 		SpringApplication.run(ElectroShopApplication.class, args);
 	}
 
