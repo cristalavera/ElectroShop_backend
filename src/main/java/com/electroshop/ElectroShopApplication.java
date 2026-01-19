f67024beac8b9abeba7d71e00e77bfb9094aebc4
cristalavera <crispipop@gmail.com>
2026-01-19 20:46:17 +0100

    Actualizar clase principal para compatibilidad con Railway sin .env

--- src/main/java/com/electroshop/ElectroShopApplication.java
+++ src/main/java/com/electroshop/ElectroShopApplication.java
@@ -2,20 +2,20 @@ package com.electroshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ElectroShopApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // lee .env automáticamente
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    public static void main(String[] args) {
+        // Solo cargar .env si no existe la variable de entorno (local)
+        if (System.getenv("DB_URL") == null) {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        }
 
-
-		SpringApplication.run(ElectroShopApplication.class, args);
-	}
-
+        SpringApplication.run(ElectroShopApplication.class, args);
+    }
 }
