7ef4c4a53dad17ebd27d75aa57e107e0949bfba2
cristalavera <crispipop@gmail.com>
2026-01-19 21:58:02 +0100

    Creación de ficheros properties independientes para dev y prod

--- src/main/java/com/electroshop/ElectroShopApplication.java
+++ src/main/java/com/electroshop/ElectroShopApplication.java
@@ -2,23 +2,10 @@ package com.electroshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ElectroShopApplication {
-
     public static void main(String[] args) {
-        // Carga .env solo si existe
-        try {
-            Dotenv dotenv = Dotenv.load();
-            System.setProperty("DB_URL", dotenv.get("DB_URL"));
-            System.setProperty("DB_USER", dotenv.get("DB_USER"));
-            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        } catch (Exception e) {
-            // Si no existe .env, asumimos que estamos en producción
-            System.out.println("No se encontró .env, usando variables de entorno del sistema.");
-        }
-
         SpringApplication.run(ElectroShopApplication.class, args);
     }
 }
