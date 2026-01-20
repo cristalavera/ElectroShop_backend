c45513ea1a13d9869c5a1d725db81042581d1348
cristalavera <crispipop@gmail.com>
2026-01-21 00:24:21 +0100

    Corrección error en la clase principal

--- src/main/java/com/electroshop/ElectroShopApplication.java
+++ src/main/java/com/electroshop/ElectroShopApplication.java
@@ -3,7 +3,7 @@ package com.electroshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class ElectroShopApplication {
