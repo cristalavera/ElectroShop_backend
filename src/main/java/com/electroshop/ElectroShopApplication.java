ee2c1674a7937c059b0e5319f32403ea3bc81cb5
cristalavera <crispipop@gmail.com>
2026-01-21 00:05:56 +0100

    dependencia dotenv y método para cargar archivo .env con las variables para local

--- src/main/java/com/electroshop/ElectroShopApplication.java
+++ src/main/java/com/electroshop/ElectroShopApplication.java
@@ -3,9 +3,12 @@ package com.electroshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ElectroShopApplication {
     public static void main(String[] args) {
+    	Dotenv dotenv = Dotenv.load(); // carga .env
         SpringApplication.run(ElectroShopApplication.class, args);
     }
 }
