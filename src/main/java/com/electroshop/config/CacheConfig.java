8c5522e2e5b3ac66623856b962c7c5ad4cbe4b2f
cristalavera <crispipop@gmail.com>
2026-01-22 18:15:32 +0100

    Agregar headers de caché

---
+++ src/main/java/com/electroshop/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package com.electroshop.config;
+
+import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+public class CacheConfig implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
+        chain.doFilter(request, response);
+    }
+}
